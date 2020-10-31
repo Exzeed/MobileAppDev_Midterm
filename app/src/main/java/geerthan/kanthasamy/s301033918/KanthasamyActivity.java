139b40eed015539ac54ad8de107022454fa81f81
Exzeed <geethan1996@hotmail.ca>
2020-10-31 08:51:48 -0400

    initial commit

---
+++ app/src/main/java/geerthan/kanthasamy/s301033918/KanthasamyActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Full Name: Geerthan Kanthasamy
+ * Student ID: 301033918
+ * Section Number: 002
+ */
+
+package geerthan.kanthasamy.s301033918;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KanthasamyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kanthasamy);
+    }
+}
